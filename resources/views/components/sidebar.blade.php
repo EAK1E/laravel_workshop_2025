eb77e0ffc5227a0092732e9eb055dd72ab5f64dd
EAK1E <pongwarith@gmail.com>
2025-04-01 11:52:07 +0700

    sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,25 +1,2 @@
 @livewire('navbar')
-<div class="sidebar">
-    <div class="sidebar-header">
-        <div class="text-center">System 1.0</div>
-    </div>
-    <div class="sidebar-body">
-        <div class="menu">
-            <ul>
-                <li>
-                    <a href="/dashboard" wire:navigate>
-                        <i class="fa-solid fa-house me-2"></i>Dashboard</li>
-                    </a>
-                <li><i class="fa-solid fa-building me-2"></i>เมนูที่ 1</li>
-                <li><i class="fa-solid fa-home me-2"></i>เมนูที่ 2</li>
-                <li><i class="fa-solid fa-user me-2"></i>เมนูที่ 3</li>
-                <li><i class="fa-solid fa-gear me-2"></i>เมนูที่ 4</li>
-
-                <li>
-                    <a href="/company/index" wire:navigate>
-                    <i class="fa-solid fa-building me-2"></i>เมนูที่ 5</li>
-                    </a>
-            </ul>
-        </div>
-    </div>
-</div>
+@livewire('sidebar')
---
+++ resources/views/livewire/sidebar.blade.php
@@ -0,0 +1,25 @@
+
+<div class="sidebar">
+    <div class="sidebar-header">
+        <div class="text-center">System 1.0</div>
+    </div>
+    <div class="sidebar-body">
+        <div class="menu">
+            <ul>
+                <li wire:click="changeMenu('dashboard')" @if ($currentMenu == 'dashboard') class="active" @endif>
+                    <a href="/dashboard" wire:navigate>
+                        <i class="fa-solid fa-house me-2"></i>Dashboard</li>
+                    </a>
+                <li wire:click="changeMenu('expense/index')" @if ($currentMenu == 'expense/index') class="active" @endif><i class="fa-solid fa-building me-2"></i>เมนูที่ 1</li>
+                <li wire:click="changeMenu('room/index')" @if($currentMenu =='room/index') class="active" @endif><i class="fa-solid fa-home me-2"></i>เมนูที่ 2</li>
+                <li wire:click="changeMenu('user/index')" @if($currentMenu == 'user/index') class="active" @endif><i class="fa-solid fa-user me-2"></i>เมนูที่ 3</li>
+                <li wire:click="changeMenu('user/index')" @if($currentMenu == 'user/index') class="active" @endif><i class="fa-solid fa-gear me-2"></i>เมนูที่ 4</li>
+
+                <li>
+                    <a href="/company/index" wire:navigate>
+                    <i class="fa-solid fa-building me-2"></i>เมนูที่ 5</li>
+                    </a>
+            </ul>
+        </div>
+    </div>
+</div>
